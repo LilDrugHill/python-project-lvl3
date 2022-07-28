11aed376119735ecd50e06a4fc58be1b7a1b9f95
LilDrugHill <101098006+LilDrugHill@users.noreply.github.com>
2022-07-28 15:50:37 +0300

    Download_links text => content

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -161,8 +161,8 @@ def links_download(
                 except requests.exceptions.ConnectionError:
                     logging.warning(f"link {link_url} was not downloaded")
                 else:
-                    with open(link_path, "w+", encoding='utf-8') as link_file:
-                        link_file.write(link_resource.text)
+                    with open(link_path, "wb") as link_file:
+                        link_file.write(link_resource.content)
 
                     create_relative_path(
                         link_tag, link_path, "href"
